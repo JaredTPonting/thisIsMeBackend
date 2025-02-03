a4cdbfb6a2a58dc971efe9e930b5b7cb0c644d8d
Jared <jaredtponting@gmail.com>
2025-02-03 19:47:01 +0000

    Fix CORS issues

--- backend/settings.py
+++ backend/settings.py
@@ -68,14 +68,24 @@ INSTALLED_APPS = [
     "blogs"
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "jtp-port-backend.herokuapp.com",
+    "http://localhost:3000"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "https://jtp-port-backend.herokuapp.com",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
