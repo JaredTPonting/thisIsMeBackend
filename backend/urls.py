5e40137463b71d6016b60b7ef0e22e1b61e061a8
Jared <jaredtponting@gmail.com>
2025-04-08 19:05:12 +0100

    new

--- backend/urls.py
+++ backend/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", lambda request: redirect("admin/", permanent=False)),
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blogs.urls"))
